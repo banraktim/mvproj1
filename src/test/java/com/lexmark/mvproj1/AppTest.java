fc0ed5bf1f010484ef4cd080ffc7d47c72078887
John Doe <john@doe.org>
2023-01-05 14:28:54 +0530

    initial commit

---
+++ src/test/java/com/lexmark/mvproj1/AppTest.java
@@ -0,0 +1,27 @@
+package com.lexmark.mvproj1;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+
+public class AppTest {
+	
+	@Test
+
+	public void testlogin1() {
+		
+		App myApp = new App();
+		Assert.assertEquals(0, myApp.userLogin("abc","abc123"));
+		
+	}
+	
+	@Test
+	public void testlogin2() {
+		
+		App myApp = new App();
+		Assert.assertEquals(1, myApp.userLogin("abc","abc@123"));
+		
+	}
+	
+
+}
